41211ade0a42f7f2af978f8645e07c112d5502a5
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2021-07-21 14:15:32 -0400

    add bg gradient

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -12,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Kanit', sans-serif;
   }
   body {
-    background-color: ${({ theme }) => theme.colors.background};
+    background-color: ${({ theme }) => theme.colors.gradients.background};
 
     img {
       height: auto;
