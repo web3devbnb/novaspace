c7be685ace2e0093c691c85b172f27ece31cc571
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2021-07-21 11:59:00 -0400

    fix imports

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from '@pancakeswap-libs/uikit'
+import { ShibanovaTheme } from '@pancakeswap-libs/uikit'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends PancakeTheme {}
+  export interface DefaultTheme extends ShibanovaTheme {}
 }
 
 const GlobalStyle = createGlobalStyle`
