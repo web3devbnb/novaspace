c408c7ab20b8cbc5db5638ad5444799d54a8652a
Diego Brito <britodiego.tech@gmail.com>
2021-07-26 18:17:08 -0400

    build: Build montserrat font

--- src/views/Lottery/components/PastDrawsHistory/HistoryChart.tsx
+++ src/views/Lottery/components/PastDrawsHistory/HistoryChart.tsx
@@ -57,7 +57,7 @@ const HistoryChart: React.FC = () => {
       borderCapStyle: 'round',
       gridLines: { display: false },
       ticks: {
-        fontFamily: 'Kanit, sans-serif',
+        fontFamily: 'Montserrat, sans-serif',
         fontColor: color,
         fontSize: 14,
         lineHeight,
