99d0e6b3f7b995cba039f7ccc2a27daf88e9735f
cristian123-rocketship <cristian123@rocketship.com>
2022-01-10 20:27:23 +0000

    Start modelling the Novaria planet grid in CSS with styled components.

--- src/views/Novaria/Novaria.tsx
+++ src/views/Novaria/Novaria.tsx
@@ -1,5 +1,26 @@
+import { times } from 'lodash'
 import React from 'react'
+import styled from 'styled-components'
 
-const Novaria = (props) => <></>
+const Grid = styled.div.attrs(props => ({ nx: 5, ny: 5 }))`
+  display: grid;
+  grid-template-columns: repeat(${props => props.nx}, 1fr);
+  grid-template-rows: repeat(${props => props.ny}, 1fr);
+`
+
+const GridCell = styled.div`
+`
+
+const Novaria = (props) => {
+    const [nx, ny] = [5, 5];
+
+    return (
+        <Grid nx={nx} ny={ny}>
+            {times(nx * ny, () => (
+                <GridCell />
+            ))}
+        </Grid>
+    )
+}
 
 export default Novaria
