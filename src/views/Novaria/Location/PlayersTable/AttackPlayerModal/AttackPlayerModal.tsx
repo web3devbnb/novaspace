7a5e6673f12bfcb6c8493f52930c849ca7053b16
cristian123-rocketship <cristian123@rocketship.com>
2022-02-07 20:59:24 +0000

    Implement an attack player model, currently empty, opened when clicking on a row in the list of players.

---
+++ src/views/Novaria/Location/PlayersTable/AttackPlayerModal/AttackPlayerModal.tsx
@@ -0,0 +1,8 @@
+import { Modal } from '@pancakeswap-libs/uikit'
+import React from 'react'
+
+const AttackPlayerModal = () => {
+  return <Modal title="ATTACK PLAYER" />
+}
+
+export default AttackPlayerModal
--- src/views/Novaria/Location/PlayersTable/PlayersTableRow/PlayersTableRow.tsx
+++ src/views/Novaria/Location/PlayersTable/PlayersTableRow/PlayersTableRow.tsx
@@ -1,12 +1,15 @@
+import { useModal } from '@pancakeswap-libs/uikit'
 import { useGetAttackPower, useGetFleetSize } from 'hooks/useNovaria'
 import React from 'react'
+import AttackPlayerModal from '../AttackPlayerModal'
 
 const PlayersTableRow = ({ fleet }) => {
   const fleetSize = useGetFleetSize(fleet)
   const fleetPower = useGetAttackPower(fleet)
+  const [handleClick] = useModal(<AttackPlayerModal />)
 
   return (
-    <div>
+    <div onClick={handleClick} onKeyDown={handleClick} role="button" tabIndex={0}>
       <div>{fleet}</div>
       <div>{fleetSize}</div>
       <div>{fleetPower}</div>
