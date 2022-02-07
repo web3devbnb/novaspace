ac3c1ebfcd0b0a16ac086c2d2beea4cc6da44b5e
cristian123-rocketship <cristian123@rocketship.com>
2022-02-07 21:07:40 +0000

    Empty battle modal.

---
+++ src/views/Novaria/Location/OpenBattlesTable/BattleModal/BattleModal.tsx
@@ -0,0 +1,8 @@
+import { Modal } from '@pancakeswap-libs/uikit'
+import React from 'react'
+
+const BattleModal = () => {
+  return <Modal title="BATTLE" />
+}
+
+export default BattleModal
--- src/views/Novaria/Location/OpenBattlesTable/OpenBattlesTableRow/OpenBattlesTableRow.tsx
+++ src/views/Novaria/Location/OpenBattlesTable/OpenBattlesTableRow/OpenBattlesTableRow.tsx
@@ -1,8 +1,12 @@
+import { useModal } from '@pancakeswap-libs/uikit'
 import React from 'react'
+import BattleModal from '../BattleModal'
 
 const OpenBattlesTableRow = ({ battle }) => {
+  const [handleClick] = useModal(<BattleModal />)
+
   return (
-    <div>
+    <div onClick={handleClick} onKeyDown={handleClick} role="button" tabIndex={0}>
       <div>{battle}</div>
       <div>0x1234...</div>
       <div>17:00 UTC</div>
