8d172da0e54f7e1a603a960ed0566f324849be27
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2022-02-15 15:58:50 -0500

    update to mainnet

--- src/views/Novaria/Location/OpenBattlesTable/BattleModal/BattleModal.tsx
+++ src/views/Novaria/Location/OpenBattlesTable/BattleModal/BattleModal.tsx
@@ -1,8 +1,28 @@
-import { Modal } from '@pancakeswap-libs/uikit'
+import { Button, Modal } from '@pancakeswap-libs/uikit'
+import ModalActions from 'components/ModalActions'
+import { useEnterBattle, useGetBattle } from 'hooks/useNovaria'
 import React from 'react'
 
-const BattleModal = () => {
-  return <Modal title="BATTLE" />
+const BattleModal = (battle, onDismiss) => {
+
+  const battleInfo = useGetBattle(battle)
+  const player = battleInfo[4][0]
+  
+
+  const { onEnterBattle } = useEnterBattle()
+  return (
+    <Modal title="BATTLE" onDismiss={onDismiss} >
+      
+      <ModalActions>
+        <Button variant="primary" onClick={() => onEnterBattle(player, 1)}>
+          ATTACK
+        </Button>
+        <Button variant="primary" onClick={() => onEnterBattle(player, 2)}>
+          DEFEND
+        </Button>
+    </ModalActions>
+    </Modal>
+  )
 }
 
 export default BattleModal
