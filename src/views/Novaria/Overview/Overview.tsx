a37cba8e580881c038ef3ead1bb75dd5021c6857
Jonathan <80490016+cryptodev999@users.noreply.github.com>
2022-02-16 11:21:14 -0600

    Basic overview page styling.

--- src/views/Novaria/Overview/Overview.tsx
+++ src/views/Novaria/Overview/Overview.tsx
@@ -17,7 +17,6 @@ import GameMenu from '../components/GameMenu'
 const Page = styled.div`
   background-image: url('/images/novaria/mapBG.jpg');
   background-size: cover;
-  font-size: 15px;
   margin-top: -105px;
   color: #5affff;
   display: flex;
@@ -33,8 +32,6 @@ const Row = styled.div`
   flex-wrap: no-wrap;
   display: flex;
 `
-
-
 const Body = styled.div`
   margin: 10px 50px 10px 150px;
   // background: #00000080;
@@ -42,7 +39,16 @@ const Body = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  text-align: center;
+  text-align: left;
+`
+
+const Header = styled.div`
+  font-size:20px;
+  padding-bottom:10px;
+`
+
+const Text = styled.div`
+  font-size:14px;
 `
 
 const Overview: React.FC = () => {
@@ -50,36 +56,33 @@ const Overview: React.FC = () => {
   const fleetLocation = useGetFleetLocation(account)
   const fleetMineral = useGetFleetMineral(account)
 
-
   return (
     <Page>
       <GameHeader location={fleetLocation} playerMineral={fleetMineral} />
       <Row>
           <GameMenu pageName="overview" />
         <Body>
-          <p>
-            Welcome to the Legend of Novaria! This is the testing phase of the game, 
+          <Header>Welcome to the Legend of Novaria</Header>
+          <Text>
+            This is the testing phase of the game, 
             so it&apos;s important to know that the game can be reset at any moment, 
-            and all progress and spent nova will be lost. The point of testing is not 
+            and all progress and in game purchases will be lost. The point of testing is not 
             to make money, but to find bugs and help us get them fixed!
-          </p><br />
-          <p>
+            <br /><br />
             Normally, the overview page would have some game stats (implemented soon) 
             and news. If you are unsure of how to start your adventure, we suggest 
             heading over to the shipyard and building some ships! You will need a fleet 
             size of at least 25 to be able to travel anywhere. 
-          </p><br />
-          <p>
+            <br /><br />
             The basic premise of the game is to build ships, travel to mining planets,
             and harvest mineral to refine into NOVA! However, the trip can be dangerous!
             The only safe location is a planet with a refinery. Everywhere else, Players 
             can attack each other and destroy each other&apos;s ships to create salvage. 
             Salvage can be collected by your mining ships and stored as mineral to also 
             be refined into NOVA. 
-          </p><br />
-          <p>
+            <br /><br />
             Good Luck!
-          </p>
+          </Text>
         </Body>
       </Row>
     </Page>
