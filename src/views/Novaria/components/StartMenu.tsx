0741c46c69161fa1d8632434af99bb65aab2b014
Jonathan <80490016+cryptodev999@users.noreply.github.com>
2022-03-07 10:24:40 -0600

    Updated register text.

--- src/views/Novaria/components/StartMenu.tsx
+++ src/views/Novaria/components/StartMenu.tsx
@@ -90,7 +90,7 @@ const StartMenu = () => {
     return (
     <div>
       <BetaWarning>
-        ***NOTICE - THIS GAME IS CURRENTLY IN BETA. Players can still earn NOVA and profit, but there is no garauntee and the game will be fully reset with no refund provided.***
+        ***NOTICE - THIS GAME IS CURRENTLY IN BETA. Players can still earn NOVA and profit, but there is no guarantee. The game will be fully reset around March 14 (no refund provided).***
       </BetaWarning>
       {!isAllowed && 'Step 1 - approve Fleet and Treasury contracts for the game'}<br />
       {allowanceFleet <= 0 ? <Button onClick={handleFleetApprove}>{!pending ? 'Approve Fleet Contract' : 'pending...'}</Button> : ''}
