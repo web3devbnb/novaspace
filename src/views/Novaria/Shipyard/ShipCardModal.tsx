6dd59946ff1c8ffdf8a9195fa80582398b367f27
cristian123-rocketship <cristian123@rocketship.com>
2022-04-02 12:47:58 +0100

    Remove unused component properties and reformat for readability.

--- src/views/Novaria/Shipyard/ShipCardModal.tsx
+++ src/views/Novaria/Shipyard/ShipCardModal.tsx
@@ -13,27 +13,25 @@ interface PlayerModalProps {
 }
 
 const Img = styled.img`
-    margin-top: 20px;
-    background-color: black;
-    border-radius: 15px;
+  margin-top: 20px;
+  background-color: black;
+  border-radius: 15px;
 
-    ${({ theme }) => theme.mediaQueries.md} {
-        min-width: 25%;
-        margin-left: auto;
-        margin-right: auto;
-    }
+  ${({ theme }) => theme.mediaQueries.md} {
+    min-width: 25%;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `
 
 const ShipCardModal: React.FC<PlayerModalProps> = ({ shipclass, onDismiss }) => {
-  
-    
   return (
-    <Modal title={shipclass} onDismiss={onDismiss} >
-      {shipclass === 'Viper' && <Img src={viperCard} alt='viper' />}
-      {shipclass === 'P.U.P.' && <Img src={moleCard} alt='viper' />}
-      {shipclass === 'Firefly' && <Img src={fireflyCard} alt='viper' />}
-      {shipclass === 'Gorian' && <Img src={gorianCard} alt='viper' />}
-      {shipclass === 'Unknown' && <Img src={unknownCard} alt='viper' />}        
+    <Modal onDismiss={onDismiss}>
+      {shipclass === 'Viper' && <Img src={viperCard} alt="viper" />}
+      {shipclass === 'P.U.P.' && <Img src={moleCard} alt="viper" />}
+      {shipclass === 'Firefly' && <Img src={fireflyCard} alt="viper" />}
+      {shipclass === 'Gorian' && <Img src={gorianCard} alt="viper" />}
+      {shipclass === 'Unknown' && <Img src={unknownCard} alt="viper" />}
     </Modal>
   )
 }
