44477d63c2e3ccfa6f3287c2ed150300fa4d7977
cristian123-rocketship <cristian123@rocketship.com>
2022-04-02 12:11:28 +0100

    Remove unused prop for battle status in the players table and re-format / re-organise the code a bit, in some places.

--- src/views/Novaria/Location/PlayersTable/PlayersTableRow/PlayersTableRow.tsx
+++ src/views/Novaria/Location/PlayersTable/PlayersTableRow/PlayersTableRow.tsx
@@ -4,38 +4,41 @@ import React from 'react'
 import styled from 'styled-components'
 import PlayerModal from '../PlayerModal'
 
-const Row = styled.div<{status: number}>`
+const Row = styled.div<{ status: number }>`
   display: grid;
   grid-template-columns: 2fr 1fr 1fr 1fr;
   border: 1px solid #289794;
   padding: 3px;
   margin: 4px 0;
-  // color:${props => props.status !== 0 ? 'gold' : '#5affff'};
   &:hover {
     background-color: #289794;
   }
 `
 const Cell = styled.div`
-  // flex: 1;
-  // width: 33%;
-  // overflow-x: hidden;
   text-overflow: ellipsis;
   text-align: right;
 `
 
-const PlayersTableRow = ({ account, refinery, player, status, currentLocation }) => {
+const PlayersTableRow = ({ account, refinery, player, currentLocation }) => {
   const fleetSize = useGetFleetSize(player)
   const fleetPower = useGetAttackPower(player)
   const name = useGetNameByAddress(player)
   const mineral = Number(useGetFleetMineral(player))
-  const [handleClick] = useModal(<PlayerModal account={account} refinery={refinery} player={player} status={status} currentLocation={currentLocation} />)
+  const [handleClick] = useModal(
+    <PlayerModal
+      account={account}
+      refinery={refinery}
+      player={player}
+      currentLocation={currentLocation}
+    />,
+  )
 
   return (
-    <Row status={player.battleStatus} onClick={handleClick} onKeyDown={handleClick} role="button" tabIndex={0} >
-      <Cell style={{textAlign:'left'}}>{name}</Cell>
+    <Row status={player.battleStatus} onClick={handleClick} onKeyDown={handleClick} role="button" tabIndex={0}>
+      <Cell style={{ textAlign: 'left' }}>{name}</Cell>
       <Cell>{fleetSize}</Cell>
       <Cell>{fleetPower}</Cell>
-      <Cell>{(mineral/10**18).toFixed(2)}</Cell>
+      <Cell>{(mineral / 10 ** 18).toFixed(2)}</Cell>
     </Row>
   )
 }
