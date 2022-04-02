849d461b42e3d993fe52ed4ffcd5fdc152fef80a
cristian123-rocketship <cristian123@rocketship.com>
2022-04-02 12:14:33 +0100

    Remove unused imports, re-format the code.

--- src/views/Novaria/components/BodyWrapper.tsx
+++ src/views/Novaria/components/BodyWrapper.tsx
@@ -1,45 +1,31 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 
 const ImgWrap = styled.div`
-    position: relative;
-    background-image: url('/images/novaria/borderTLBR.png'), url('/images/novaria/borderTR.png'), url('/images/novaria/borderTLBR.png'), url('/images/novaria/borderBL.png');
-    background-position: top 3px left 3px, top 5px right 10px, bottom 3px right 3px, bottom 5px left 10px;
-    // background-size: 100% 100%;
-    background-repeat: no-repeat;
-    background-color: #00000080;
-    
+  position: relative;
+  background-image: url('/images/novaria/borderTLBR.png'), url('/images/novaria/borderTR.png'),
+    url('/images/novaria/borderTLBR.png'), url('/images/novaria/borderBL.png');
+  background-position: top 3px left 3px, top 5px right 10px, bottom 3px right 3px, bottom 5px left 10px;
+  background-repeat: no-repeat;
+  background-color: #00000080;
 `
 
 const ChildWrapper = styled.div`
-    margin: 10px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    padding-bottom: 10px;
-   
-    border: 2px solid #a0bbd5;
-    
-    position: relative;
-
+  margin: 10px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding-bottom: 10px;
+  border: 2px solid #a0bbd5;
+  position: relative;
 `
 
-
-
-
-const BodyWrapper = ({children}) => {
-
-    return (
-       
-        <ImgWrap>
-            <ChildWrapper>
-                
-                  {children}
-                
-            </ChildWrapper>
-        </ImgWrap>
-       
-    )
+const BodyWrapper = ({ children }) => {
+  return (
+    <ImgWrap>
+      <ChildWrapper>{children}</ChildWrapper>
+    </ImgWrap>
+  )
 }
 
 export default BodyWrapper
